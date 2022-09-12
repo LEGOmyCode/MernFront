20c19ee8a8d860122d1e7268d56c775be82bb85c
Raymond Mojica <rm@M12020.home>
2022-09-12 01:02:18 -0400

    Landing page looks better

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,10 +4,10 @@ import "./styles.css";
 
 const Home = () => {
   return (
-    <>
+    <div className="landingPage">
       <h1 className="title">Welcome to MERN'd Black</h1>
       <SearchWindow />
-    </>
+    </div>
   );
 };
 
